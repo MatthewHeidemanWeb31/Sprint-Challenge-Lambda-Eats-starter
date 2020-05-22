ffeffaae60f41fa62803628ac15636f3236b7e7b
matthew-heideman <heidemanmr@yahoo.com>
2020-05-22 10:58:16 -0500

    Set-Up Routes and Started Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from 'styled-components';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import PizzaForm from './components/PizzaForm';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      <Switch>
+        <Route path = '/pizza'>
+          <PizzaForm />
+        </Route>
+        <Route path = '/'>
+          <HomePage />
+        </Route>
+      </Switch>
     </>
   );
 };
---
+++ src/components/PizzaForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PizzaForm(){
+    return (
+        <>
+        </>
+    )
+}
+export default PizzaForm;
